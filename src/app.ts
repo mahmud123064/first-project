46cbcbb43620e1dea7a7b7d61e3c5ffab915bd8c
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2024-05-22 10:25:58 +0600

    basic setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+const port = 3000;
+
+// Parsers
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  var a = 10;
+  res.send(a);
+});
+
+// console.log(process.cwd());
+
+export default app;
